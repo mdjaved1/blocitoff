8798290faba0ec869df8ca33b5623c7e04339dba
Muhammad <daudjaved1@gmail.com>
2017-08-01 10:34:36 +0000

    revision 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    if !current_user.nil?
+       @user = current_user
+       @item = Item.new
+       @items = @user.items
+     else
   end
 end
