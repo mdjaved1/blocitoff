b7f53a8c53f8e37a49afde02b5ad39389c07cb13
Muhammad <daudjaved1@gmail.com>
2017-08-07 22:40:50 +0000

    completed display time and refactor

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,16 +4,15 @@ class ItemsController < ApplicationController
   end
   
   def create
-     @item = Item.new(item_params)
-     @item.user = current_user
- 
-     if @item.save
-       flash[:notice] = "Item was saved."
-       redirect_to current_user
-     else
-       flash[:error] = "There was an error saving the item. Please try again."
-       render :new
-     end
+    @item = current_user.items.new(item_params)
+
+    if @item.save
+      flash[:notice] = "Item was saved."
+      redirect_to root_path
+    else
+      flash[:error] = "There was an error saving the item. Please try again."
+      render :new
+    end
   end
   
   def destroy 
@@ -23,13 +22,13 @@ class ItemsController < ApplicationController
       flash[:notice] = "Item was completed"
     else
       flash[:error] = "There was an error saving the item. Please try again."
-      
     end
+     redirect_to root_path
   end  
   
   private 
   
   def item_params 
-    params.require(:item).permit(:name, :user)
+    params.require(:item).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
 
-
-  get 'users/show'
-
   devise_for :users
   
-  get 'home/index'
-  
+  resources :users, only: :show
   root 'users#show'
-  
-  resources :users do 
-    resources :items, only: [:new, :create]
-  end
-  
   resources :items
   
 end
